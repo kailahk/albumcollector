5b4f72e91f1485603cd332827cd101f3190b8aa7
kailahk <kailahkorsh@me.com>
2022-11-15 12:48:32 -0800

    model creation and migration, superuser creation

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Album(models.Model):
+    title = models.CharField(max_length=100)
+    artist = models.CharField(max_length=100)
+    genres = models.CharField(max_length=200)
+    release_year = models.IntegerField()
+    track_list = models.TextField(max_length=250)
+    list = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.title
--- main_app/views.py
+++ main_app/views.py
@@ -1,11 +1,5 @@
 from django.shortcuts import render
-
-# baby step - usually a model is used
-albums = [
-    {'name': 'To Pimp a Butterfly', 'artist': 'Kendrick Lamar', 'list': 'All Time Favorites'},
-    {'name': 'The Score', 'artist': 'The Fugees', 'list': 'All Time Favorites'},
-    {'name': 'Crash', 'artist': 'Charli XCX', 'list': 'Best of 2022'},
-]
+from .models import Album
 
 # Create your views here.
 def home(request):
@@ -15,6 +9,7 @@ def about(request):
     return render(request, 'about.html')
 
 def albums_index(request):
+    albums = Album.objects.all()
     return render(request, 'albums/index.html', {
         'albums': albums
     })
