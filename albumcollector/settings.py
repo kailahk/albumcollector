ea0daa89649b360c29ee2c7296801a3a9a447956
kailahk <kailahkorsh@me.com>
2022-11-28 09:56:10 -0800

    Config deployment

--- albumcollector/settings.py
+++ albumcollector/settings.py
@@ -126,8 +126,11 @@ LOGIN_REDIRECT_URL = '/albums/'
 
 LOGOUT_REDIRECT_URL = '/'
 
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+import django_on_heroku
+django_on_heroku.settings(locals())
